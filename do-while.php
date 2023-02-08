ef1519846e3f46add15aebac42f51f6652b60beb
dev-rahim <rahimuddin5915@gmail.com>
2023-02-08 20:02:42 +0600

    exploring php while and do while loop

---
+++ do-while.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Do While</title>
+</head>
+<body>
+
+
+    <?php
+    $a=1;
+    do {
+        
+       
+       
+?>
+<button style="width: 120px; padding: 10px 0; margin-bottom: 50px; margin-right: 30px;"><?php echo $a;?></button>
+<?php   $a++;
+  } while ($a<=100);
+
+     ?>
+
+</body>
+</html>
